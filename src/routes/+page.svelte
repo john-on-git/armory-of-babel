51b055f8e23f25e286bb3883b0fe7f8f670edf7c
John Gregg-Webster <johngreggwebster@gmail.com>
2025-10-22 18:42:06 +0100

    weapons no longer regen on refresh (real)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -69,7 +69,9 @@
             }
         }
 
-        const newQuery = `?${searchParams.toString()}`;
+        const queryNoQuestion = searchParams.toString();
+        const newQuery =
+            queryNoQuestion.length > 0 ? `?${queryNoQuestion}` : "";
         if (window.location.search !== newQuery) {
             // and update the URL params to point to its ID
             tick().then(() => replaceState(newQuery, {}));
