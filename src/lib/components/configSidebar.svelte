22cc6ef9f2d02053b9778b8244f432aadcb8eb78
John Gregg-Webster <johngreggwebster@gmail.com>
2025-10-22 18:42:04 +0100

    reset button

--- src/lib/components/configSidebar.svelte
+++ src/lib/components/configSidebar.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { defaultWeaponRarityConfigFactory } from "$lib/generators/weaponGenerator/weaponGeneratorConfigLoader";
     import { type WeaponRarityConfig } from "$lib/generators/weaponGenerator/weaponGeneratorTypes";
     import { type Writable } from "svelte/store";
     import RaritySlider from "./raritySlider.svelte";
@@ -8,18 +9,35 @@
         configWritable: Writable<WeaponRarityConfig>;
     }
 
-    const { configWritable: _configWritable }: Props = $props();
+    const { configWritable }: Props = $props();
 
     let rarityOdds: [number, number, number, number] = $state([
         0.1, 0.333, 0.666, 0.9,
     ]);
+
+    function resetToDefault() {
+        configWritable.set(defaultWeaponRarityConfigFactory());
+    }
 </script>
 
 <Sidebar localStorageKey={"weaponConfigSidebar"}>
-    <h2>Generation Parameters</h2>
-
-    <RaritySlider bind:odds={rarityOdds} />
+    <div class="config-flex">
+        <h2>Custom Generation Parameters</h2>
+        <button onclick={resetToDefault}>🗑️</button>
+        <div>
+            <RaritySlider bind:odds={rarityOdds} />
+        </div>
+    </div>
 </Sidebar>
 
 <style>
+    .config-flex {
+        height: 100%;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: start;
+        gap: 1rem;
+    }
 </style>
