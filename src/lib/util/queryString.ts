96c919a4138c85f64479b9b5c9bd7372351a76a9
John Gregg-Webster <johngreggwebster@gmail.com>
2025-10-22 18:42:07 +0100

    history modifcation issues

--- src/lib/util/queryString.ts
+++ src/lib/util/queryString.ts
@@ -1,14 +1,24 @@
-import { replaceState } from "$app/navigation";
+import { pushState, replaceState } from "$app/navigation";
 import { tick } from "svelte";
 
-export function pushURLSearchParamsToLocation(searchParams: URLSearchParams) {
+export function syncLocationWithURLSearchParams(searchParams: URLSearchParams, mode: 'push' | 'replace') {
     const queryNoQuestion = searchParams.toString();
     const newQuery =
         queryNoQuestion.length > 0 ? `?${queryNoQuestion}` : "";
     if (window.location.search !== newQuery) {
         // and update the URL params to point to its ID
         tick().then(() => {
-            replaceState(newQuery, {});
+            switch (mode) {
+                case 'push':
+                    pushState(newQuery, {});
+                    break;
+                case 'replace':
+                    replaceState(newQuery, {});
+                    break;
+                default:
+                    mode satisfies never;
+            }
+
             tick().then(() => dispatchEvent(new PopStateEvent("popstate", { state: null })));
         });
     }
