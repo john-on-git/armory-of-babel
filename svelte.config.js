991b5da445512b1f2752e686e8408a074af14d7f
John Gregg-Webster <johngreggwebster@gmail.com>
2025-10-22 18:42:03 +0100

    workflow changes

--- svelte.config.js
+++ svelte.config.js
@@ -14,8 +14,8 @@ const config = {
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			pages: 'output',
-			assets: 'output',
+			pages: 'build',
+			assets: 'build',
 			fallback: undefined,
 			precompress: false,
 			strict: true
