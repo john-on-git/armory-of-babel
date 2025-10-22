3696b91f797aaea9c530c6e870996bc7de359019
John Gregg-Webster <johngreggwebster@gmail.com>
2025-10-22 18:42:10 +0100

    file organisation

---
+++ src/lib/generators/weaponGenerator/config/v2.ts
@@ -0,0 +1,108 @@
+import { ProviderElement } from "$lib/generators/weaponGenerator/provider";
+import type { ActivePower, PassivePower, WeaponAdjective, WeaponFeaturesTypes, WeaponPowerCond } from "$lib/generators/weaponGenerator/weaponGeneratorTypes";
+import type { DeltaCollection } from "$lib/util/versionController";
+
+export default {
+    shapes: {},
+    adjectives: {
+        add: [
+            new ProviderElement<WeaponAdjective, WeaponPowerCond>('pale', { desc: 'pale' }, { themes: { any: ['light'] } }),
+        ]
+    },
+    actives: {
+        add: [
+            new ProviderElement<ActivePower, WeaponPowerCond>('frostbound', {
+                desc: 'Bind',
+                cost: 1,
+                additionalNotes: [
+                    'Lock a mechanism in place with magical ice as strong as steel.',
+                    'It stays frozen for 2d6 × 10 minutes.'
+                ]
+            }, {
+                themes: {
+                    any: ['ice']
+                }
+            }),
+            new ProviderElement<ActivePower, WeaponPowerCond>('rally-person', {
+                desc: 'Rally',
+                cost: 1,
+                additionalNotes: [
+                    'Targets one non-hostile character.',
+                    'For the rest of the day, their morale cannot break.'
+                ]
+            }, {
+                themes: {
+                    any: ['light', 'steampunk']
+                }
+            }),
+            new ProviderElement<ActivePower, WeaponPowerCond>('homing-shot', {
+                desc: 'Homing Shot',
+                cost: 1,
+                additionalNotes: [
+                    'Fire an enchanted shot, which always hits.'
+                ]
+            }, {
+                shapeFamily: {
+                    any: ['sword (or bow)', 'dagger (or pistol)', 'greatsword (or musket)']
+                }
+            }),
+            new ProviderElement<ActivePower, WeaponPowerCond>('binding-shot', {
+                desc: 'Binding Shot',
+                cost: 2,
+                additionalNotes: [
+                    'Fire an enchanted shot, which anchors the target to a nearby surface.',
+                    'They are stuck in place until they use their turn to successfully save and escape.'
+                ]
+            }, {
+                themes: { any: ['nature', 'ice'] },
+                shapeFamily: {
+                    any: ['sword (or bow)', 'dagger (or pistol)', 'greatsword (or musket)']
+                }
+            }),
+            new ProviderElement<ActivePower, WeaponPowerCond>('black-flame-blast', {
+                desc: 'Black Flame Blast',
+                cost: 3,
+                additionalNotes: [
+                    'Summon a 20-ft cone of black flame, which deals 4d6 damage.',
+                    'Damage inflicted by black flames can only be healed by magic.'
+                ]
+            }, {
+                themes: { all: ['dark', 'fire'] }
+            })
+        ]
+    },
+    passives: {
+        add: [
+            new ProviderElement<PassivePower, WeaponPowerCond>('weapon-permanently-invisible',
+                {
+                    miscPower: true,
+                    desc: 'The weapon is completely invisible, except to its wielder.'
+                },
+                {
+                    themes: {
+                        any: ['light']
+                    },
+                    rarity: {
+                        gte: 'epic'
+                    },
+                    isSentient: true // If it can't call out to you, how will you know it's there?
+                }
+            ),
+            new ProviderElement<PassivePower, WeaponPowerCond>('instant-recall',
+                {
+                    miscPower: true,
+                    desc: "The wielder can summon the weapon into their hand at will, so long as it's on the same plane."
+                },
+                {
+                    rarity: {
+                        gte: 'uncommon'
+                    },
+                }
+            )
+        ]
+    },
+    languages: {},
+    themes: {},
+    personalities: {},
+    rechargeMethods: {},
+} satisfies DeltaCollection<WeaponFeaturesTypes>;
