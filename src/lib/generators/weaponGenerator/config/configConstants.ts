08cf6991cea6f9222dad01bee9f10ded637079ca
John Gregg-Webster <johngreggwebster@gmail.com>
2025-10-22 18:42:12 +0100

    revert

--- src/lib/generators/weaponGenerator/config/configConstants.ts
+++ src/lib/generators/weaponGenerator/config/configConstants.ts
@@ -3,5 +3,6 @@ import type { WeaponShapeGroup } from "$lib/generators/weaponGenerator/weaponGen
 export const edgedWeaponShapeFamilies = ['dagger', 'sword', 'greatsword', 'axe', 'greataxe', 'polearm', 'sword (or bow)', 'dagger (or pistol)', 'sword (or musket)', 'greataxe (or musket)'] as const satisfies WeaponShapeGroup[];
 export const bluntWeaponShapeFamilies = ['club', 'mace', 'staff'] as const satisfies WeaponShapeGroup[];
 export const pointedWeaponShapeFamilies = ['spear', 'lance'] as const;
+export const sharpWeaponShapeFamiliesArr = [...edgedWeaponShapeFamilies, ...pointedWeaponShapeFamilies];
 
 export const swordlikeWeaponShapeFamilies = ['dagger', 'sword', 'greatsword', 'sword (or bow)', 'dagger (or pistol)', 'sword (or musket)'] satisfies WeaponShapeGroup[];
