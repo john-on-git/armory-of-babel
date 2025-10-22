5a79abca3a2e99bf0282e2a0d579744aca3e833d
John Gregg-Webster <johngreggwebster@gmail.com>
2025-10-22 18:42:04 +0100

    vercel config

--- svelte.config.js
+++ svelte.config.js
@@ -12,11 +12,6 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: vercel(),
-		paths: {
-			base: ''
-			// line below is used when deploying to GitHub pages
-			//base: process.argv.includes('dev') ? '' : '/tetragrade/weapon-generator'
-		}
 	}
 };
 
